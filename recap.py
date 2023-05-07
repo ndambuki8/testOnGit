27a705b3e97756d7b109ab6409685455fab86f75
ndambuki8 <ndambukikevo8@gmail.com>
2023-05-07 15:33:51 +0300

    Converting km to miles

--- recap.py
+++ recap.py
@@ -1,5 +1,13 @@
-import random
+#Python program to convert KIlometers to Miles
 
-randomVal = random.randint(1,100)
+#Taking the kilometers as input from the user
 
-print("Random value is: ", randomVal)
+km = float(input("Enter value in kilometers:"))
+
+#Conversion factor constant
+conv_fac = 0.621371
+
+#Calculate miles
+miles = km * conv_fac
+
+print("%0.5f kilometers is equal to  %0.5f miles"%(km, miles))
