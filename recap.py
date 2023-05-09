ae8708766aea5e1c10dd9a3bbfb3cb2e05201421
ndambuki8 <ndambukikevo8@gmail.com>
2023-05-09 17:52:45 +0300

    Celsius to Fahrenheit

--- recap.py
+++ recap.py
@@ -1,13 +1,7 @@
-#Python program to convert KIlometers to Miles
+#Python program to convert Temperature from Celsius To Fahrenheit
 
-#Taking the kilometers as input from the user
+celsius = 37.5
 
-km = float(input("Enter value in kilometers:"))
-
-#Conversion factor constant
-conv_fac = 0.621371
-
-#Calculate miles
-miles = km * conv_fac
-
-print("%0.5f kilometers is equal to  %0.5f miles"%(km, miles))
+#calculate farhenheit
+fa = (celsius * 1.8) + 32
+print('%0.1f degree celsius is equal to %0.1f degree Fahrenheit'%(celsius, fa))
