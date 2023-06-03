7e7659af775c9e271a52b621cc84591e489949f0
ndambuki8 <ndambukikevo8@gmail.com>
2023-06-03 07:10:04 +0300

    Factorial of number using for loop

--- recap.py
+++ recap.py
@@ -1,15 +1,14 @@
-#Python program to dislpay all the prime numbers within an interval
+#Factorial of a number using a for loop
 
-lower = 900
-upper = 1000
+num = 6
 
-print("Pime numbers between ", lower, " and ", upper, " are: ")
+factorial = 1
 
-for num in range(lower, upper + 1):
-    #All prime numbers are greater than 1
-    if num > 1:
-        for i in range(2, num):
-            if (num % i) == 0:
-                break
-        else:
-            print(num)
+if num < 0:
+    print("Fact of a num less than 0 haiko")
+elif num == 0:
+    print("The fact of ", num, " is 1")
+else:
+    for i in range(1, num+1):
+        factorial = factorial * i
+    print("The factorial of ", num, " is ", factorial)
