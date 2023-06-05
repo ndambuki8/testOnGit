8ab7dd65ddadd083c57f36cdec3bffe23769f32f
ndambuki8 <ndambukikevo8@gmail.com>
2023-06-05 07:48:15 +0300

    factorial using recursion

--- recap.py
+++ recap.py
@@ -1,14 +1,14 @@
-#Factorial of a number using a for loop
+#Factorial of a number using recursion
 
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return (x * factorial(x-1))
+
+#Change the value for a different result
 num = 6
 
-factorial = 1
+result = factorial(num)
 
-if num < 0:
-    print("Fact of a num less than 0 haiko")
-elif num == 0:
-    print("The fact of ", num, " is 1")
-else:
-    for i in range(1, num+1):
-        factorial = factorial * i
-    print("The factorial of ", num, " is ", factorial)
+print("The factorial of", num, "is",result)
