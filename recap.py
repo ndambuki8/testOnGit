1b105e8d66e97160f2c13d377356388568315da6
ndambuki8 <ndambukikevo8@gmail.com>
2023-06-07 06:02:27 +0300

    fibonacci series

--- recap.py
+++ recap.py
@@ -1,6 +1,21 @@
-#Python program to display the multiplication table
+#Python program to print the fibonacci sequence
 
-num = 12
+nterms = int(input("How many terms? "))
 
-for i in range(1, 11):
-    print(num, 'x', i, '=', num * i)
+n1, n2 = 0, 1 #first two terms
+count = 0
+
+#check if the nuber of terms is valid
+if nterms <= 0:
+    print("Please enter a positive integer")
+elif nterms == 1:
+    print("Fibonacci sequence upto", nterms, ":", n1)
+else:
+    print("Fibonacci sequence:")
+    while count < nterms:
+        print(n1)
+        nth = n1 + n2
+        #update values
+        n1 = n2
+        n2 = nth
+        count+=1
