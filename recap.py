60d7529f3bbd1462e9bced1f40f88bf79d51db96
ndambuki8 <ndambukikevo8@gmail.com>
2023-06-08 07:28:45 +0300

    getting the power value from length of the number

--- recap.py
+++ recap.py
@@ -2,13 +2,15 @@
 
 num = int(input("Enter a number: "))
 
+n = len(str(num))
+
 sum = 0
 
 temp = num
 while temp > 0:
     digit = temp % 10
 
-    sum += digit ** 3
+    sum += digit ** n
     temp //= 10
 
 if num == sum:
