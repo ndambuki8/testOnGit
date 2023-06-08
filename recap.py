362e3878d1f345efa46fa66fdcc247e35741d4ab
ndambuki8 <ndambukikevo8@gmail.com>
2023-06-08 07:27:14 +0300

    Armstrong check logic

--- recap.py
+++ recap.py
@@ -1,21 +1,17 @@
-#Python program to print the fibonacci sequence
+#Python program to Check Armstrong Number
 
-nterms = int(input("How many terms? "))
+num = int(input("Enter a number: "))
 
-n1, n2 = 0, 1 #first two terms
-count = 0
+sum = 0
 
-#check if the nuber of terms is valid
-if nterms <= 0:
-    print("Please enter a positive integer")
-elif nterms == 1:
-    print("Fibonacci sequence upto", nterms, ":", n1)
+temp = num
+while temp > 0:
+    digit = temp % 10
+
+    sum += digit ** 3
+    temp //= 10
+
+if num == sum:
+    print(num, "is an ArmStrong number")
 else:
-    print("Fibonacci sequence:")
-    while count < nterms:
-        print(n1)
-        nth = n1 + n2
-        #update values
-        n1 = n2
-        n2 = nth
-        count+=1
+    print(num, "is not an ArmStrong number")   
