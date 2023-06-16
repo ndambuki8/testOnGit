05841a6d97ae6d214836c89eac47cd6dcf32ce0c
John Ndambuki Kelvin <34053595+ndambuki8@users.noreply.github.com>
2023-06-16 12:02:39 +0300

    Update recap.py

--- recap.py
+++ recap.py
@@ -1,9 +1,3 @@
 #Python program to Find the sum of Natral numbers
-N = int(input("TYpe upper limit of natural numbers: "))
-
-sum = 0
-
-for i in range(0, N+1):
-    sum += i
-
-print(sum)
+for i in [1,2,3,4]:
+   print(i)
