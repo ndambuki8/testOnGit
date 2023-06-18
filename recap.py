6b1f51e5eb838bb7f9610c6ee1e308eff1196768
ndambuki8 <ndambukikevo8@gmail.com>
2023-06-18 10:47:14 +0300

    diffrent sum of N numbers algorithm

--- recap.py
+++ recap.py
@@ -1,9 +1,12 @@
 #Python program to Find the sum of Natral numbers
-N = int(input("TYpe upper limit of natural numbers: "))
+num = int(input("TYpe upper limit of natural numbers: "))
 
-sum = 0
-
-for i in range(0, N+1):
-    sum += i
-
-print(sum)
+if num < 0:
+    print("Enter a positive number: ")
+else:
+    sum = 0
+    #use while loop to iterate until zero
+    while (num > 0):
+        sum += num
+        num -= 1
+    print("THe sum is", sum)
