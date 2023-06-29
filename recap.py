2fe8c31b8ed039adc1aac4ba1e598fdd77c34fa6
ndambuki8 <ndambukikevo8@gmail.com>
2023-06-30 00:00:58 +0300

    HCF/GCD

--- recap.py
+++ recap.py
@@ -1,4 +1,17 @@
-#Python program to find the ASCII Value of the given character
+#Python program to find the HCF or GCD
 
-c = 'A'
-print("The ASCII value of '" + c + "' is", ord(c))
+def compute_hcf(x, y):
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller+1):
+        if((x % i == 0) and (y % i == 0)):
+            hcf = i
+
+    return hcf
+
+num1 = 12
+num2 = 14
+
+print("The HCF is", compute_hcf(num1, num2))
