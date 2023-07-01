8440cff807a9877bad43517cf166a460211aa5f0
ndambuki8 <ndambukikevo8@gmail.com>
2023-07-02 00:57:16 +0300

    GCD using eucledian distance

--- recap.py
+++ recap.py
@@ -1,17 +1,8 @@
-#Python program to find the HCF or GCD
+#Python program to find the HCF or GCD using Eucledean algorithm
 
 def compute_hcf(x, y):
-    if x > y:
-        smaller = y
-    else:
-        smaller = x
-    for i in range(1, smaller+1):
-        if((x % i == 0) and (y % i == 0)):
-            hcf = i
-
-    return hcf
-
-num1 = 12
-num2 = 14
-
-print("The HCF is", compute_hcf(num1, num2))
+    while(y):
+        x, y = y, x % y
+    return x
+hcf = compute_hcf(300,400)
+print("hcf is", hcf)
