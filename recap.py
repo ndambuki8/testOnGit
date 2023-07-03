575149c68c153674c60ea7f9217f1fb0fdef1e73
ndambuki8 <ndambukikevo8@gmail.com>
2023-07-03 23:16:36 +0300

    LCM

--- recap.py
+++ recap.py
@@ -1,8 +1,21 @@
-#Python program to find the HCF or GCD using Eucledean algorithm
+#Python program to find the LCM
+def compute_lcm(x, y):
 
-def compute_hcf(x, y):
-    while(y):
-        x, y = y, x % y
-    return x
-hcf = compute_hcf(300,400)
-print("hcf is", hcf)
+    #choose the greater number
+    if x > y:
+        greater = x
+    else:
+        greater = y
+
+    while(True):
+        if((greater % x == 0) and (greater % y == 0)):
+            lcm = greater
+            break
+        greater += 1
+
+    return lcm
+
+num1 = 24
+num2 = 54
+
+print(compute_lcm(num1, num2))
