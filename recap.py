5b46d053c9e08c04a28170da935f77770fd093c9
ndambuki8 <ndambukikevo8@gmail.com>
2023-07-05 09:56:18 +0300

    LCM using GCD

--- recap.py
+++ recap.py
@@ -1,4 +1,14 @@
-y = [1,2,3,4,5]
-def stack(x, y):
+#LCM using GCD
+def compute_gcd(x,y):
+    while(y):
+          x, y = y, x % y
+    return x
 
-    
+def compute_lcm(x, y):
+     lcm = (x*y)//compute_gcd(x,y)
+     return lcm
+
+num1 = 54
+num2 = 24
+
+print("The LCM is", compute_lcm(num1, num2))
